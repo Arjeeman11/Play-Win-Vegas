96d2a319ca41aa9198bb9d1985b6653fc019c014
Armand Joseph Ebitner <armandebitner11@gmail.com>
2025-06-21 14:35:28 +0800

    Update firebase.js

--- src/•  src/firebase.js
+++ src/•  src/firebase.js
@@ -1,2 +1,31 @@
-// Add your firebase config here
-export const db = {};
+// Replace the config below with your own Firebase credentials
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDrcKirva3JbojDoallhpyvhAsY6OS6cfM",
+  authDomain: "play-win-vegas.firebaseapp.com",
+  projectId: "play-win-vegas",
+  storageBucket: "play-win-vegas.firebasestorage.app",
+  messagingSenderId: "894751883552",
+  appId: "1:894751883552:web:785dd45fdef55c88901745",
+  measurementId: "G-NGC61J7RVY"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+
