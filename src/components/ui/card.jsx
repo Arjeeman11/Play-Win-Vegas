c14decb917019231679181ec971eeaaf497e0360
Armand Joseph Ebitner <armandebitner11@gmail.com>
2025-06-21 13:14:36 +0800

    Create card.jsx

---
+++ src/components/ui/card.jsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function Card({ className = "", children }) {
+  return <div className={`rounded-2xl shadow-lg ${className}`}>{children}</div>;
+}
+
+export function CardContent({ className = "", children }) {
+  return <div className={`p-4 ${className}`}>{children}</div>;
+}
